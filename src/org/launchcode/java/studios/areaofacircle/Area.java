d1d39439a0566559236a19e24e1509d18e170165
irynaprofile <105556785+irynaprofile@users.noreply.github.com>
2023-01-05 19:28:45 -0600

    exercises and studio1

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -5,8 +5,22 @@ public class Area {
     public static void main(String[] args) {
 
        Scanner input = new Scanner(System.in);
-       System.out.print("Enter a radius: ");
-       double radius = input.nextDouble();
+        double radius;
+
+        do {
+           System.out.print("Enter a radius: ");
+
+           if (!input.hasNextDouble()) {
+               System.out.println("Error! Invalid input.");
+               return;
+           }
+
+           radius = input.nextDouble();
+
+           if (radius < 0) {
+               System.out.println("Error! Invalid input.");
+           }
+       } while (radius < 0);
 
        //double area = Math.PI * radius *radius;
         double area = Circle.getArea(radius);
